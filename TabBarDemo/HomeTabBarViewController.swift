eae7599ced5ef72c0f4ea02c5c85f3d45f65ea96
nikhil <nikhil.nangia85@gmail.com>
2018-09-26 12:32:17 +0530

    1. Added Three View Controller For Tab bar 2. Added Tabbar controller file 3. Removed Main.Storyboard and it's reference in info.plist 4. Set Window Root element in AppDelegate.swift

---
+++ TabBarDemo/HomeTabBarViewController.swift
@@ -0,0 +1,53 @@
+//
+//  HomeTabBarViewController.swift
+//  Homingos
+//
+//  Created by Admin on 9/26/18.
+//  Copyright © 2018 DemoApp. All rights reserved.
+//
+
+import UIKit
+
+class HomeTabBarViewController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let userVc = UserViewController()
+        userVc.tabBarItem = UITabBarItem(tabBarSystemItem: .contacts, tag: 0)
+        userVc.title = "Users"
+        userVc.view.backgroundColor = UIColor.orange
+        
+        let galleryVc = GalleryViewController()
+        galleryVc.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
+        galleryVc.title = "Gallery"
+        galleryVc.view.backgroundColor = .blue
+        
+        let infoVc = InfoViewController()
+        infoVc.tabBarItem = UITabBarItem(tabBarSystemItem: .more, tag: 2)
+        infoVc.title = "Info"
+        infoVc.view.backgroundColor = .red
+        
+        let tabBarList = [userVc, galleryVc,infoVc]
+        
+        //        viewControllers = tabBarList
+        viewControllers = tabBarList.map { UINavigationController(rootViewController: $0)}
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
